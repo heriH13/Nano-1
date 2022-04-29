6a546d7c6dec5313b952e7dd3735f66aafae2d3f
heriH13 <51155814+heriH13@users.noreply.github.com>
2022-04-29 13:55:05 +0700

    update

--- Nano1/Nano1/SceneDelegate.swift
+++ Nano1/Nano1/SceneDelegate.swift
@@ -17,12 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        window = UIWindow(windowScene: windowScene)
-//        let vc = UIStoryboard(name: "Shoes", bundle: nil)
-//        let root = vc.instantiateViewController(identifier: "ShoesSB")
-//        window?.rootViewController = root
-//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
